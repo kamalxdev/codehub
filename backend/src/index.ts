815637c872ffa5f841f10c27874d54b0cee1d636
Kamal Singh <itskamal.dev@gmail.com>
2024-04-10 21:30:32 +0530

    Completed basic setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,16 @@
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, { cors: { origin: "*" }});
 
 
-console.log("Hello World");
+
+io.on("connection", (socket) => {
+    console.log("A user connected: ", socket.id);
+});
+
+httpServer.listen(3000,()=> console.log("Server is running on port 3000"));
+
---
+++ frontend/src/pages/home.tsx
@@ -0,0 +1,12 @@
+
+
+function Home() {
+  return (
+    <div>
+      <h1>Home gguigujg</h1>
+    </div>
+  );
+}
+
+
+export default Home;
